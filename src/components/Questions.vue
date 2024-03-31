6c0dee3a2d0f474a4d3f828af6437ba295498b8b
Jeff-dev31 <jahbuchim@bepeerless.co>
2024-03-31 11:31:21 +0100

    completed test

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -17,24 +17,28 @@ import { useBuilderStore } from '../stores/builder'
 import { storeToRefs } from 'pinia'
 
 
-const { openBuilder, selectedQuestion } = storeToRefs(useBuilderStore())
+const { openBuilder, selectedQuestion, questions: assessmentQuestions } = storeToRefs(useBuilderStore())
 
 const setSelectedQuestion = (question: Questions) => {
     openBuilder.value = false
     selectedQuestion.value = question
     openBuilder.value = true
 }
+
+const deleteQuestion = (id: string) => {
+    assessmentQuestions.value = assessmentQuestions.value.filter((question) => question.id !== id)
+}
 </script>
 
 <template>
-    <Card @click="setSelectedQuestion(questions)" class="mb-2 hover:bg-slate-50 cursor-pointer">
+    <Card class="mb-2 hover:bg-slate-50 cursor-pointer">
         <CardHeader>
             <div class="flex items-center justify-between border-b pb-[5px] border-[#DBDCDE]">
                 <p class="text-[#00AD1B] font-medium text-[16px]">{{ index + 1 }}</p>
                 <Icon icon="line-md:grid-3-filled" class="text-[#98A2B3] cursor-pointer text-[18px]" />
             </div>
         </CardHeader>
-        <CardContent>
+        <CardContent  @click="setSelectedQuestion(questions)">
             <div class="flex items-start gap-x-2">
                 <Icon icon="material-symbols-light:check-box-outline"
                     class="text-[#404a5a] cursor-pointer text-[30px]" />
@@ -49,7 +53,7 @@ const setSelectedQuestion = (question: Questions) => {
                 </div>
                 <div class="flex items-center gap-x-2">
                     <Icon icon="lets-icons:copy-light" class="text-[16px] text-[#475467] cursor-pointer" />
-                    <Icon icon="solar:trash-bin-minimalistic-linear"
+                    <Icon @click="deleteQuestion(questions.id)" icon="solar:trash-bin-minimalistic-linear"
                         class="text-[16px] text-[#475467] cursor-pointer" />
                 </div>
             </div>
